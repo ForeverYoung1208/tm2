12ba1dba78ce026d2d912263866222ab056b15b8
ForeverYoung1208 <siafin2010@gmail.com>
2014-10-06 00:18:21 +0300

    не работает так как мешанина с датами - ид шки присваиваться могут не хронологически по отношнию к thedate

--- app/controllers/stat_controller.rb
+++ app/controllers/stat_controller.rb
@@ -1,13 +1,10 @@
 class StatController < ApplicationController
 
 	def index
-		@date_begin ||= Time.now.to_date
-		@date_end ||= Time.now.to_date+1.day
+		@date_begin ||= Time.now.to_date-1.day
+		@date_end ||= Time.now.to_date
 
-		join_str = "LEFT OUTER JOIN `adrivers` ON `adrivers`.`id` = `aorders`.`adriver_id`"
-		odate_first = Odate.find_by_thedate(@date_begin)  блять ебучая проблма поиска первой Одаты
-		odate_last = Odate.find_by_thedate(@date_last)     и последней одаты. и нахера я вообще это лепил?
-      	@aorders = Aorder.joins(join_str).order(session[:sort_orders_by]).where("odate_id > #{odate_first.id} AND odate_id < #{odate_last.id} AND iscanceled=false")
+		get_orders 
 
 	    respond_to do |format|
 	      format.html # index.html.erb
@@ -19,10 +16,26 @@ class StatController < ApplicationController
 		@date_begin = params[:first_date]
 		@date_end = params[:last_date]
 
+		get_orders 
+
 	    respond_to do |format|
 	      format.html { render :index }# index.html.erb
 	      format.xml  { }
 	    end
+	end
+
+	private
+
+	def get_orders 
+		odate_first_id = Odate.find_by_thedate(@date_begin).id
+		odate_last_id = Odate.find_by_thedate(@date_end).id
+
+		#debugger
+
+		join_str = "LEFT OUTER JOIN `adrivers` ON `adrivers`.`id` = `aorders`.`adriver_id`"
+
+      	@aorders = Aorder.joins(join_str).order(session[:sort_orders_by]).where("odate_id >= #{odate_first_id} AND odate_id <= #{odate_last_id} AND iscanceled=false")
+
 	end
 
 
