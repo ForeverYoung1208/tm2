313a341513548633334ba2a1c3635d4ad7cc3021
Igor <siafin2010@gmail.com>
2015-02-28 00:28:55 +0200

    1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,13 @@ module ApplicationHelper
   end
 
 
+  def td_odometer(odobegin, odoend, outofcity, args={}, &block )
+    classes='hoverable'
+    classes+=' badodometer' if (odoend-odobegin-outofcity)>100 or (odoend-odobegin-outofcity)<=0
+    args[:class] = classes
+    content_tag(:td, args, &block )
+  end
+
+
+
 end
