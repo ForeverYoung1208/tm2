dca8709edae7a33de4a6ca5d500d084e9aef003d
Igor <siafin2010@gmail.com>
2015-04-02 11:04:58 +0300

    1

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ config.time_zone = 'Kyiv'
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
 
 config.assets.paths << "vendor"
 
